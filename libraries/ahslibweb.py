28ddaccd5ddb9fbfa58eaef1f8dead45206a7833
luckyele <541913141@qq.com>
2019-01-29 00:41:02 +0800

    ahslibweb.py ok

--- libraries/ahslibweb.py
+++ libraries/ahslibweb.py
@@ -25,9 +25,9 @@ class Web(Webmonkey):
 		super().__init__(self.url, self.website)
 
 	def get_obj(self):
-		para = {'page':1,'PageNum':15, 'RootNav':1306}
+		para = {'page':1,'pageSize':15, 'RootNav':1036}
 		rels = []
-		surl = ""
+		surl = "http://www.ahlib.com/v-AhLibWeb-zh_CN-/AhLibWeb/main/mainActivity.w?url=../news/newsDetail.w&id="
 		header = {
 			"Accept":"application/json, text/javascript, */*; q=0.01",
 			"Accept-Encoding":"gzip, deflate",
@@ -37,14 +37,16 @@ class Web(Webmonkey):
 			"Content-Type":"application/json",
 			"Host":"www.ahlib.com",
 			"Referer":"http://www.ahlib.com/v-AhLibWeb-zh_CN-/AhLibWeb/mian.w?mainid=4",
-			"User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64;rv:64.0) Gecko/20100101 Firefox/64.0",
+			"User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0",
 			"X-Requested-With":"XMLHttpRequest",
 		}
 		res = requests.post(self.url, headers=header, data=json.dumps(para))
-		
-		print(len(json.loads(res.text)))
-		if res.status_code == '200':
-			print(json.loads(res))
+		l = json.loads(res.text)
+		for a in l['List']:
+			time = a['AddTime'][0:10]
+			title = a['title']
+			url = surl + str(a['id'])
+			rels.append((time, title, url))
 		return rels
 
 	def get_newest_message(self, obj):
